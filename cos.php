8a73eafa21ff473cad233d6f7643f18b7ed0e59c
Estudante Marcela (Ceep) <125074470+marcyvessaro@users.noreply.github.com>
2024-03-08 09:32:05 -0300

    Add files via upload

---
+++ cos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hipotenusa</title>
+</head>
+<body>
+
+<?php
+
+echo cos(M_PI); // -1
+
+?>
+
+   
+ <br>
+    <a href= "http://localhost/funçoes/index.html">voltar</a>
+</body>
+</html>
